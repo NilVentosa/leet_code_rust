1a2824211196e023713f9f8a98c21a289332eebb
Nil Ventosa <nil.ventosa@sword-group.com>
2022-08-13 12:02:10 +0200

    Add two_sum

---
+++ two_sum/src/main.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+fn main() 
+{
+    println!("{:?}", two_sum(vec![1,2,3,4,5], 9));
+}
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> 
+{
+    let mut old: HashMap<i32,i32> = HashMap::new();
+
+    for (i, num) in nums.iter().enumerate() {
+        match old.get(num) {
+             Some(&index) => return vec![index, i as i32],
+             None => old.insert(target - num, i as i32),
+         };
+    }
+
+    vec![]
+}
+
+#[cfg(test)]
+ mod tests {
+    use crate::two_sum;
+     #[test]
+     fn one() {
+         let result = vec![3,4];
+         assert_eq!(result, two_sum(vec![1,2,3,4,5], 9))
+     }
+
+     #[test]
+     fn two() {
+         let result: Vec<i32> = vec![];
+         assert_eq!(result, two_sum(vec![1,3,9,5], 9))
+     }
+
+     #[test]
+     fn three() {
+         let result: Vec<i32> = vec![0,3];
+         assert_eq!(result, two_sum(vec![1,2,9,8], 9))
+     }
+
+ }
